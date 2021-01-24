460ec747b01db8081a25ab19f43580913d557a16
kelber <kelberstuchi@gmail.com>
2021-01-24 19:36:04 -0300

    working project with new title

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ const routes: Routes = [
   },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: '**', component: RegisterComponent },
 ];
 
 @NgModule({
